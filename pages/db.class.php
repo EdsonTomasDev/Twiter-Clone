4466482a1230afcbac04de8cc5cea5978f2b13a2
Tomas Edson <t.edson.almeida@hotmail.com>
2022-03-09 20:19:01 -0300

    criado a classe db para conexão com o banco de dados

---
+++ pages/db.class.php
@@ -0,0 +1,37 @@
+<?php
+
+class db{
+
+    //host
+    private $host = '127.0.0.1';
+
+    //usuario
+    private $usuario = 'tom'
+
+    //senha
+    private $senha = '1234$*';
+
+    //banco de dados
+    private $database = 'twitter_clone';
+
+    public function conecta_mysql(){
+
+        //criar conexão
+       $con = mysqli_connect($this->host, $this->usuario, $this->senha, $this->database);
+
+        //ajustar o charset de comunicação entre a aplicação e o banco de dados
+        mysqli_set_charset($con, 'utf8');
+
+        //Verificar se há erro de conexão
+        if(mysqli_connect_errno()){
+            echo 'Erro ao tentar se conectar com o banco de dados MYSQL: '. mysqli_connect_error();
+        }
+
+        return $con;
+    }
+
+}
+
+
+
+?>
