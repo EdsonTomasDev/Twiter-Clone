39661ca3cb5e750aaebd47c937a16f913b595338
Tomas Edson <t.edson.almeida@hotmail.com>
2022-04-09 19:43:48 -0300

    config exibição do botão seguir e deixar de seguir

--- pages/procurar_pessoas.php
+++ pages/procurar_pessoas.php
@@ -36,6 +36,9 @@
 							$('#pessoas').html(data);
 								$('.btn_seguir').click(function(){
 									let id_usuario = $(this).data('id_usuario');
+									//ATIVAR OU DESATIVAR A EXIBIÇÃO DO BOTÃO CASO SEJA CLICADO
+									$('#btn_seguir_' + id_usuario).hide();
+									$('#btn_deixar_seguir_' + id_usuario).show();
 										$.ajax({
 											url: 'seguir.php',
 											method: 'post',
@@ -50,6 +53,9 @@
 
 								$('.btn_deixar_seguir').click(function(){
 									let id_usuario = $(this).data('id_usuario');
+									//ATIVAR OU DESATIVAR A EXIBIÇÃO DO BOTÃO CASO SEJA CLICADO
+									$('#btn_seguir_' + id_usuario).show();
+									$('#btn_deixar_seguir_' + id_usuario).hide();
 										$.ajax({
 											url: 'deixar_seguir.php',
 											method: 'post',
